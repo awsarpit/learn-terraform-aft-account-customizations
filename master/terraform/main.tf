23eba8ebfa2785f0ae2b4085534b068dcd8b5558
awsarpit <122075856+awsarpit@users.noreply.github.com>
2024-01-12 14:41:22 +0530

    Update main.tf

--- master/terraform/main.tf
+++ master/terraform/main.tf
@@ -1,20 +1,3 @@
-data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      identifiers = ["cloudformation.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
-  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy.json
-  name               = "AWSCloudFormationStackSetAdministrationRole"
-}
-
 resource "aws_cloudformation_stack_set" "ec2-deploy" {
   auto_deployment {
     enabled                          = true
@@ -29,22 +12,9 @@ resource "aws_cloudformation_stack_set" "ec2-deploy" {
     ImageId = ""
   }
 
+  permission_model = "SERVICE_MANAGED"
+  
   capabilities = ["CAPABILITY_IAM","CAPABILITY_NAMED_IAM","CAPABILITY_AUTO_EXPAND"]
 
   template_body = file("../templates/ec2.yml")
 }
-
-data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
-  statement {
-    actions   = ["sts:AssumeRole"]
-    effect    = "Allow"
-    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.ec2-deploy.execution_role_name}"]
-  }
-}
-
-resource "aws_iam_role_policy" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
-  name   = "ExecutionPolicy"
-  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy.json
-  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.name
-}
-
