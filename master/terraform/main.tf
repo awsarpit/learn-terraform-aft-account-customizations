c85b90ea9b572cbc0b26c1f76ca36ef45119fef4
awsarpit <122075856+awsarpit@users.noreply.github.com>
2024-01-12 14:58:08 +0530

    Update main.tf

--- master/terraform/main.tf
+++ master/terraform/main.tf
@@ -6,15 +6,21 @@ resource "aws_cloudformation_stack_set" "ec2-deploy" {
   name = "ec2-deploy"
 
   parameters = {
-    SubnetId = "",
-    SecurityGroupIds = "",
-    KeyName = "",
-    ImageId = ""
+    SubnetId = "subnet-08bb2534bb0f9b445",
+    SecurityGroupIds = "sg-09532d763841e1a44",
+    KeyName = "ap-south-1",
+    ImageId = "ami-0d3f444bc76de0a79"
   }
 
   permission_model = "SERVICE_MANAGED"
-  
+
   capabilities = ["CAPABILITY_IAM","CAPABILITY_NAMED_IAM","CAPABILITY_AUTO_EXPAND"]
 
   template_body = file("../templates/ec2.yml")
+}
+
+resource "aws_cloudformation_stack_set_instance" "ec2-deploy-instance" {
+  account_id     = "637423245454"
+  region         = "ap-south-1"
+  stack_set_name = aws_cloudformation_stack_set.ec2-deploy.name
 }
