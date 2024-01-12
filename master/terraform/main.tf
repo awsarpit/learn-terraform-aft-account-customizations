178668e173120f28b4c593511b02e67a4c3f4459
awsarpit <122075856+awsarpit@users.noreply.github.com>
2024-01-12 15:29:54 +0530

    Update main.tf

--- master/terraform/main.tf
+++ master/terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_cloudformation_stack_set" "ec2-deploy" {
 
 resource "aws_cloudformation_stack_set_instance" "ec2-deploy-instance" {
   deployment_targets {
-    organizational_unit_ids = "ou-cjb1-2svsbfk4"
+    organizational_unit_ids = ["ou-cjb1-2svsbfk4"]
   }
   region         = "ap-south-1"
   stack_set_name = aws_cloudformation_stack_set.ec2-deploy.name
